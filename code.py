9bb64dd253fd9723feab00b147b461ceeb4f8006
Aditya Bharadwaj <aditya21nov@gmail.com>
2021-11-23 18:37:36 -0800

    Construct Aligned Strings

--- code.py
+++ code.py
@@ -1,8 +1,9 @@
 import tracemalloc
 import sys
 import time
-import numpy as np
 import re
+import numpy as np
+
 """ mismatch_penalties = \
 [
  [0, 110, 48, 94],
@@ -17,7 +18,7 @@ mismatch_penalties = {
     "C": {    "A": 110,   "C": 0,      "G": 118,    "T": 48    },
     "G": {    "A": 48,    "C": 118,    "G": 0,      "T": 110    },
     "T": {    "A": 94,    "C": 48,      "G": 110,    "T": 0    }
-} 
+}
 
 
 delta = 30
@@ -39,13 +40,13 @@ def process_input(filename):
     """
     with open(filename, 'r') as input_txt:
         input_lines = [line.rstrip('\n') for line in input_txt.readlines()]
-    
+
     x_base = None
     x_indices = list()
 
     y_base = None
     y_indices = list()
-    
+
     for line in input_lines:
         if not x_base:
             if line.isalpha():
@@ -59,68 +60,103 @@ def process_input(filename):
             else:
                 y_indices.append(int(line))
     assert x_base and y_base, "Base strings cannot be NoneType"
-    
     valid_pattern = re.compile('[AGTCagtc]*')
-    assert re.match(valid_pattern, x_base) and re.match(valid_pattern, y_base), "String contains invalid characters!" 
+    assert re.match(valid_pattern, x_base) and re.match(valid_pattern, y_base), "String contains invalid characters!"
     dna_str_x = generate_string(x_base, x_indices)
     dna_str_y = generate_string(y_base, y_indices)
     return (dna_str_x.upper(), dna_str_y.upper())
 
+def verify_output(X, Y, output_file):
+    with open(output_file, 'r') as of:
+        values = [line.rstrip() for line in of.readlines()]
+    X_o_f50 = values[0][:50]
+    X_o_l50 = values[0][-50:]
+    Y_o_f50 = values[1][:50]
+    Y_o_l50 = values[1][-50:]
+
+
+
 def calculate_alignment_cost(X,Y):
     x_len = len(X)
     y_len = len(Y)
-    alignment_cost = np.empty((x_len, y_len))
-    
-    for i in range(0, x_len):
+    alignment_cost = np.empty((x_len+1, y_len+1))
+
+    for i in range(0, x_len+1):
         alignment_cost[i][0] = i * delta
 
-    for j in range(y_len):
+    for j in range(y_len+1):
         alignment_cost[0][j] = j * delta
 
-    for i in range(1,x_len):
-        for j in range(1, y_len):
-            # print(X[:i], Y[:j], mismatch_penalties[X[i]][Y[j]])
+    for i in range(1,x_len+1):
+        for j in range(1,y_len+1):
             alignment_cost[i][j] = \
                 min(
-                    alignment_cost[i-1][j-1] + mismatch_penalties[X[i]][Y[j]],
+                    alignment_cost[i-1][j-1] + mismatch_penalties[X[i-1]][Y[j-1]],
                     alignment_cost[i-1][j] + delta,
-                    alignment_cost[i][j-1] + delta 
+                    alignment_cost[i][j-1] + delta
                 )
     return alignment_cost
 
 
 def create_aligned_sequence(alignment_cost, X, Y):
-    i = len(X)-1
-    j = len(Y)-1
+    i = len(X)
+    j = len(Y)
     aligned_X = str()
     aligned_Y = str()
-    
-    while i >= 0 and j >= 0:
+    pointer_moves = 0
+    while i != 0 and j != 0:
         # print(X[i], Y[j],i , j)
-        if(X[i] == Y[j]):
-            aligned_X += X[i]
-            aligned_Y += Y[j]
+        if X[i-1] == Y[j-1]:
+            aligned_X += X[i-1]
+            aligned_Y += Y[j-1]
             i -= 1
             j -= 1
-        elif(alignment_cost[i][j] == alignment_cost[i-1][j-1] + mismatch_penalties[X[i]][Y[j]]):
-            aligned_X += X[i]
-            aligned_Y += Y[j]
+        elif alignment_cost[i][j] == (alignment_cost[i-1][j-1] + mismatch_penalties[X[i-1]][Y[j-1]]):
+            aligned_X += X[i-1]
+            aligned_Y += Y[j-1]
             i -= 1
             j -= 1
-        elif(alignment_cost[i][j] == alignment_cost[i-1][j] + delta):
-            aligned_X += X[i]
+        elif alignment_cost[i][j] == (alignment_cost[i-1][j] + delta):
+            aligned_X += X[i-1]
             aligned_Y += '_'
-            i -=  1
-        elif(alignment_cost[i][j] == alignment_cost[i][j-1] + delta):
+            i -= 1
+        elif alignment_cost[i][j] == (alignment_cost[i][j-1] + delta):
             aligned_X += '_'
-            aligned_Y += Y[j]
+            aligned_Y += Y[j-1]
             j -=  1
-    
-    if j > 0:
-        aligned_X += '_' * (j+1)
-    if i > 0:
-        aligned_Y += '_' * (i+1)
 
+    # Option 1:
+    # l = i + j
+    # xl = len(aligned_X)
+    # yl = len(aligned_Y)
+    # print(l, xl, yl)
+    # while xl < l:
+    #     if i > 0:
+    #         aligned_X += X[i-1]
+    #         i -= 1
+    #     else:
+    #         aligned_X += '_'
+    #     xl += 1    
+    # while yl < l:
+    #     if j > 0:
+    #         aligned_Y += Y[j-1]
+    #         j -= 1
+    #     else:
+    #         aligned_Y += '_'
+    #     yl += 1
+
+    # ptr = l-1
+    # while aligned_X[ptr] == '_' and aligned_Y[ptr] == '_':
+    #     ptr -= 1
+    # return aligned_X[ptr::-1], aligned_Y[ptr::-1]
+
+    # Option 2:
+    if i > 0:
+        aligned_X += X[:i]
+        aligned_Y += '_' * i
+    if j > 0:
+        aligned_Y += Y[:j]
+        aligned_X += '_' * j
     return aligned_X[::-1], aligned_Y[::-1]
 
 if __name__ == '__main__':
@@ -129,13 +165,15 @@ if __name__ == '__main__':
         sys.exit()
     start_time = time.time()
     tracemalloc.start()
-    # X,Y = process_input(sys.argv[1])
-    X = 'CTGA'
-    Y = 'AGCT'
-    print(X,Y)
-    alc = calculate_alignment_cost(X,Y)
-    print(alc, alc[len(X)-1][len(Y)-1])
-   #  print(create_aligned_sequence(alc, X, Y))
+    X_orig,Y_orig = process_input(sys.argv[1])
+    alignment_cost_matrix = calculate_alignment_cost(X_orig,Y_orig)
+    print("Optimal Alignment Cost:", alignment_cost_matrix[len(X_orig)][len(Y_orig)])
+    # print(X,Y)
+    X_a, Y_a = create_aligned_sequence(alignment_cost_matrix, X_orig, Y_orig)
+    print(len(X_a), len(Y_a))
+    print(X_orig, Y_orig)
+    print(X_a, Y_a)
+    # verify_output(X_a, Y_a, 'BaseTestcases_CS570FinalProject/output1.txt')
     print(tracemalloc.get_traced_memory())
     tracemalloc.stop()
     print(" --- Finished in %s seconds --- " % (time.time() - start_time))
